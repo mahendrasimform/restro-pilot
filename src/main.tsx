d30b04a6aa43a74340cb393380a5a7d995c91a7c
mahendrasimform <mahendra.kakadiya@simformsolutions.com>
2025-05-31 16:50:50 +0530

    added sentry setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,36 @@ import { Provider } from "react-redux";
 import { store } from "src/store";
 import { ConfigProvider } from "antd";
 import theme from "src/theme/themeAntd";
-
+import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  tracesSampleRate: 0.5,
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost"],
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <ConfigProvider theme={theme}>
         <App />
+        <button
+          onClick={() => {
+            throw new Error("This is your first error!");
+          }}
+        >
+          Break the world
+        </button>
+        ;
       </ConfigProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  // Load environment variables from .env file
+  return {
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        org: "simform-up",
+        project: "restro-pilot",
+        authToken: env.VITE_APP_SENTRY_AUTH_TOKEN,
+      }),
+    ],
 
-  // additional configuration for absolute path
-  resolve: {
-    alias: {
-      src: "/src",
+    // additional configuration for absolute path
+    resolve: {
+      alias: {
+        src: "/src",
+      },
     },
-  },
+
+    build: {
+      sourcemap: true,
+    },
+  };
 });
